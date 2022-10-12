34b6ea16b4673d46821b137a7389a5a94046daaa
Premkumar <premkumaralaguraj@gmail.com>
2022-10-12 14:52:29 +0530

    print writer changes

--- src/main/java/com/premsh/webxmlapp/Webxmlapp.java
+++ src/main/java/com/premsh/webxmlapp/Webxmlapp.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Webxmlapp {
 	public Webxmlapp() {
-		System.out.println();
+		System.out.println("Bean Created");
 	}
 	@GetMapping("/")
 	public ResponseEntity<String> index(){
