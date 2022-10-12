2f0d19ca7afef81bb006096ba40781e908c0d5c7
Premkumar <premkumaralaguraj@gmail.com>
2022-10-12 12:36:39 +0530

    initial commit

---
+++ src/main/java/com/premsh/webxmlapp/Webxmlapp.java
@@ -0,0 +1,17 @@
+package com.premsh.webxmlapp;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Webxmlapp {
+	public Webxmlapp() {
+		System.out.println();
+	}
+	@GetMapping("/")
+	public ResponseEntity<String> index(){
+		System.out.println("I am triggered");
+		return ResponseEntity.ok("Helow hii !!");
+	}
+}
